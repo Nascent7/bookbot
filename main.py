0e5271c81ada116a662dafdb0578bf42154ec627
Nascent7 <mahquintana@gmail.com>
2025-04-19 19:56:49 -0400

    first read function and print to console

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        contents =  f.read()
+        return contents
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+main()
